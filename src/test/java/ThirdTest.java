ffe6aa17754a1183297855f84ca345cdbab403cd
tester <lookbugs@mail.ru>
2021-09-28 12:32:04 +0300

    added ThirdTest.java

---
+++ src/test/java/ThirdTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ThirdTest {
+
+    @Test
+    void thirdTest() {
+        Assertions.assertTrue(true);
+    }
+
+}
